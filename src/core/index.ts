88447929adeea9868e40583d6e711534640eeb05
Victoria Johns <victoria.johns@groupbyinc.com>
2017-06-21 16:29:13 -0400

    Add area

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,10 +4,14 @@ import Adapters from './adapters';
 import * as Events from './events';
 import Observer from './observer';
 import reducer from './reducers';
+import { DEFAULT_AREA } from './reducers/data/area';
+import { DEFAULT_COLLECTION } from './reducers/data/collections';
 import Selectors from './selectors';
 import Store, { ReduxStore } from './store';
 
 export {
+  DEFAULT_AREA,
+  DEFAULT_COLLECTION,
   ActionCreator,
   Actions,
   Adapters,
