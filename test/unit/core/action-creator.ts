2c701e81f5f260e71bd48bce0e245a23d57a8316
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-26 17:35:59 -0400

    support selectRefinement action

--- test/unit/core/action-creator.ts
+++ test/unit/core/action-creator.ts
@@ -144,29 +144,33 @@ suite('ActionCreator', ({ expect, spy, stub }) => {
     });
 
     describe('addRefinement()', () => {
-      const action = { a: 'b' };
-
-      it('should update a value refinement when two parameters are provided', () => {
+      it('should add a refinement', () => {
+        const action = { a: 'b' };
+        const refinement = { c: 'd' };
         const navigationId = 'book';
         const value = 'a';
         const updateSearch = stub(actions, 'updateSearch').returns(action);
+        const refinementPayload = stub(utils, 'refinementPayload').returns(refinement);
 
         const result = actions.addRefinement(navigationId, value);
 
         expect(result).to.be.eq(action);
-        expect(updateSearch).to.be.calledWithExactly({ navigationId, value });
+        expect(updateSearch).to.be.calledWithExactly(refinement);
       });
+    });
 
-      it('should update a range refinement when three parameters are provided', () => {
+    describe('switchRefinement()', () => {
+      it('should clear the refinements and add a refinement', () => {
         const navigationId = 'book';
-        const low = 0;
-        const high = 1;
+        const value = 'a';
+        const action = { a: 'b' };
         const updateSearch = stub(actions, 'updateSearch').returns(action);
+        const refinementPayload = stub(utils, 'refinementPayload').returns({ c: 'd' });
 
-        const result = actions.addRefinement(navigationId, low, high);
+        const result = actions.switchRefinement(navigationId, value);
 
         expect(result).to.be.eq(action);
-        expect(updateSearch).to.be.calledWithExactly({ navigationId, low, high, range: true });
+        expect(updateSearch).to.be.calledWithExactly({ c: 'd', clear: navigationId });
       });
     });
 
