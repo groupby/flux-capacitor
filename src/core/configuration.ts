e808c476abd7087d4156d269e3cd48269b7d5253
Declan Zhao <zydc2010@gmail.com>
2017-08-15 13:43:37 -0400

    Add baseUrl in config

--- src/core/configuration.ts
+++ src/core/configuration.ts
@@ -47,6 +47,11 @@ interface Configuration {
    * network request configuration
    */
   network?: Configuration.Bridge;
+
+  /**
+   * state initial configuration for Base URL
+   */
+  baseUrl?: string;
 }
 
 namespace Configuration {
