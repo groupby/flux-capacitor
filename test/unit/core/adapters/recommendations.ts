455a89540314b51384da1782611e943796ddac5b
Mengqi <mengqi.zhu@groupbyinc.com>
2017-09-27 15:13:50 -0400

    add test for pinNavigations and pinRefinement

--- test/unit/core/adapters/recommendations.ts
+++ test/unit/core/adapters/recommendations.ts
@@ -1,12 +1,48 @@
-import Adapter from '../../../../src/core/adapters/recommendations';
+import ConfigurationAdapter from '../../../../src/core/adapters/configuration';
+import RecommendationsAdapter from '../../../../src/core/adapters/recommendations';
 import suite from '../../_suite';
 
-suite('Recommendations Adapter', ({ expect }) => {
+suite('Recommendations Adapter', ({ expect, stub }) => {
 
   describe('buildUrl()', () => {
     it('should build the request URL', () => {
       // tslint:disable-next-line max-line-length
-      expect(Adapter.buildUrl('myCustomer','a','b')).to.eq('https://myCustomer.groupbycloud.com/wisdom/v2/public/recommendations/a/_getb');
+      expect(RecommendationsAdapter.buildUrl('myCustomer', 'a', 'b')).to.eq('https://myCustomer.groupbycloud.com/wisdom/v2/public/recommendations/a/_getb');
+    });
+  });
+
+  describe('pinNavigations() ', () => {
+    it('should pin navigations', () => {
+      const results: any = [{ name: 'a' }, { name: 'b' }, { name: 'c' }];
+      const extractNavigationsPinned = stub(ConfigurationAdapter, 'extractNavigationsPinned')
+        .returns(['b']);
+      expect(RecommendationsAdapter.pinNavigations(results, <any>{}))
+        .to.be.eql([{ name: 'b' }, { name: 'a' }, { name: 'c' }]);
+    });
+  });
+
+  describe('pinRefinements() ', () => {
+    it('should pin refinements', () => {
+      const results: any = [{
+        name: 'a',
+        refinements: [{ value: '1' }, { value: '2' }, { value: '3' }]
+      },
+      {
+        name: 'b',
+        refinements: [{ value: '1' }, { value: '2' }, { value: '3' }]
+      }
+      ];
+      const extractRefinementsPinned = stub(ConfigurationAdapter, 'extractRefinementsPinned')
+        .returns({ a: ['2', '3'] });
+      expect(RecommendationsAdapter.pinRefinements(results, <any>{}))
+        .to.be.eql([{
+          name: 'a',
+          refinements: [{ value: '2' }, { value: '3' }, { value: '1' }]
+        },
+        {
+          name: 'b',
+          refinements: [{ value: '1' }, { value: '2' }, { value: '3' }]
+        }]);
     });
   });
 });
