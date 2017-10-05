d419d1c03dde8d6814d659289b47dce141856d42
Victoria Johns <victoria.johns@groupbyinc.com>
2017-10-05 15:31:55 -0400

    Put events in namespace

--- src/core/events.ts
+++ src/core/events.ts
@@ -1,86 +1,90 @@
-export const ORIGINAL_QUERY_UPDATED = 'original_query_updated'; // pre
-export const CORRECTED_QUERY_UPDATED = 'corrected_query_updated'; // post
-export const RELATED_QUERIES_UPDATED = 'related_queries_updated'; // post
-export const DID_YOU_MEANS_UPDATED = 'did_you_means_updated'; // post
-export const QUERY_REWRITES_UPDATED = 'query_rewrites_updated'; // post
+namespace Events {
+  export const ORIGINAL_QUERY_UPDATED = 'original_query_updated'; // pre
+  export const CORRECTED_QUERY_UPDATED = 'corrected_query_updated'; // post
+  export const RELATED_QUERIES_UPDATED = 'related_queries_updated'; // post
+  export const DID_YOU_MEANS_UPDATED = 'did_you_means_updated'; // post
+  export const QUERY_REWRITES_UPDATED = 'query_rewrites_updated'; // post
 
-// sort events
-export const SORTS_UPDATED = 'sorts_updated'; // mixed
+  // sort events
+  export const SORTS_UPDATED = 'sorts_updated'; // mixed
 
-// product events
-export const PRODUCTS_UPDATED = 'products_updated'; // mixed
-export const MORE_PRODUCTS_ADDED = 'more_products_added'; // post
+  // product events
+  export const PRODUCTS_UPDATED = 'products_updated'; // mixed
+  export const MORE_PRODUCTS_ADDED = 'more_products_added'; // post
 
-// collection events
-export const COLLECTION_UPDATED = 'collection_updated'; // post
-export const SELECTED_COLLECTION_UPDATED = 'selected_collection_updated'; // post
+  // collection events
+  export const COLLECTION_UPDATED = 'collection_updated'; // post
+  export const SELECTED_COLLECTION_UPDATED = 'selected_collection_updated'; // post
 
-// navigation events
-export const NAVIGATIONS_UPDATED = 'navigations_updated'; // post
-export const SELECTED_REFINEMENTS_UPDATED = 'selected_refinements_updated'; // post
+  // navigation events
+  export const NAVIGATIONS_UPDATED = 'navigations_updated'; // post
+  export const SELECTED_REFINEMENTS_UPDATED = 'selected_refinements_updated'; // post
 
-// autocomplete events
-export const AUTOCOMPLETE_QUERY_UPDATED = 'autocomplete_query_updated'; // pre
-export const AUTOCOMPLETE_SUGGESTIONS_UPDATED = 'autocomplete_suggestions_updated'; // post
-export const AUTOCOMPLETE_PRODUCTS_UPDATED = 'autocomplete_products_updated'; // post
-export const AUTOCOMPLETE_TEMPLATE_UPDATED = 'autocomplete_template_updated'; // post
+  // autocomplete events
+  export const AUTOCOMPLETE_QUERY_UPDATED = 'autocomplete_query_updated'; // pre
+  export const AUTOCOMPLETE_SUGGESTIONS_UPDATED = 'autocomplete_suggestions_updated'; // post
+  export const AUTOCOMPLETE_PRODUCTS_UPDATED = 'autocomplete_products_updated'; // post
+  export const AUTOCOMPLETE_TEMPLATE_UPDATED = 'autocomplete_template_updated'; // post
 
-// template events
-export const TEMPLATE_UPDATED = 'template_updated'; // post
+  // template events
+  export const TEMPLATE_UPDATED = 'template_updated'; // post
 
-// details events
-export const DETAILS_UPDATED = 'details_updated'; // pre
-export const DETAILS_PRODUCT_UPDATED = 'details_product_updated'; // post
+  // details events
+  export const DETAILS_UPDATED = 'details_updated'; // pre
+  export const DETAILS_PRODUCT_UPDATED = 'details_product_updated'; // post
 
-// page events
-export const PAGE_UPDATED = 'page_updated'; // post
-export const PAGE_SIZE_UPDATED = 'page_size_updated'; // pre
-export const CURRENT_PAGE_UPDATED = 'current_page_updated'; // pre
+  // page events
+  export const PAGE_UPDATED = 'page_updated'; // post
+  export const PAGE_SIZE_UPDATED = 'page_size_updated'; // pre
+  export const CURRENT_PAGE_UPDATED = 'current_page_updated'; // pre
 
-// record count event
-export const RECORD_COUNT_UPDATED = 'record_count_updated'; // post
+  // record count event
+  export const RECORD_COUNT_UPDATED = 'record_count_updated'; // post
 
-// request state change events
-export const RECALL_CHANGED = 'recall_changed';
-export const SEARCH_CHANGED = 'search_changed';
+  // request state change events
+  export const RECALL_CHANGED = 'recall_changed';
+  export const SEARCH_CHANGED = 'search_changed';
 
-// redirect event
-export const REDIRECT = 'redirect';
+  // redirect event
+  export const REDIRECT = 'redirect';
 
-// recommendations events
-export const RECOMMENDATIONS_PRODUCTS_UPDATED = 'recommendations_products_updated';
+  // recommendations events
+  export const RECOMMENDATIONS_PRODUCTS_UPDATED = 'recommendations_products_updated';
 
-// error events
-export const ERROR_BRIDGE = 'error:bridge';
-export const ERROR_FETCH_ACTION = 'error:fetch_action';
+  // error events
+  export const ERROR_BRIDGE = 'error:bridge';
+  export const ERROR_FETCH_ACTION = 'error:fetch_action';
 
-// ui events
-export const UI_UPDATED = 'ui:updated';
+  // ui events
+  export const UI_UPDATED = 'ui:updated';
 
-// app events
-export const APP_STARTED = 'app:started';
-export const APP_KILLED = 'app:killed';
+  // app events
+  export const APP_STARTED = 'app:started';
+  export const APP_KILLED = 'app:killed';
 
-// location events
-export const LOCATION_UPDATED = 'location:updated';
+  // location events
+  export const LOCATION_UPDATED = 'location:updated';
 
-// tracker events
-export const BEACON_SEARCH = 'beacon:search';
-export const BEACON_VIEW_PRODUCT = 'beacon:view_product';
-export const BEACON_ADD_TO_CART = 'beacon:add_to_cart';
-export const BEACON_REMOVE_FROM_CART = 'beacon:remove_from_cart';
-export const BEACON_VIEW_CART = 'beacon:view_cart';
-export const BEACON_ORDER = 'beacon:order';
+  // tracker events
+  export const BEACON_SEARCH = 'beacon:search';
+  export const BEACON_VIEW_PRODUCT = 'beacon:view_product';
+  export const BEACON_ADD_TO_CART = 'beacon:add_to_cart';
+  export const BEACON_REMOVE_FROM_CART = 'beacon:remove_from_cart';
+  export const BEACON_VIEW_CART = 'beacon:view_cart';
+  export const BEACON_ORDER = 'beacon:order';
 
-// observer events
-export const OBSERVER_NODE_CHANGED = 'observer:node_changed';
+  // observer events
+  export const OBSERVER_NODE_CHANGED = 'observer:node_changed';
 
-// tag events
-export const TAG_LIFECYCLE = 'tag:lifecycle';
-export const TAG_ALIASING = 'tag:aliasing';
+  // tag events
+  export const TAG_LIFECYCLE = 'tag:lifecycle';
+  export const TAG_ALIASING = 'tag:aliasing';
 
-// history events
-export const HISTORY_SAVE = 'history:save';
+  // history events
+  export const HISTORY_SAVE = 'history:save';
 
-// url events
-export const URL_UPDATED = 'url:updated';
+  // url events
+  export const URL_UPDATED = 'url:updated';
+}
+
+export default Events;
