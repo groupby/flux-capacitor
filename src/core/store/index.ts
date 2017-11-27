a20d03f701244c6e083cfa9354d5c23156a5d398
Mengqi <mengqi.zhu@groupbyinc.com>
2017-11-27 14:43:21 -0500

    remove type

--- src/core/store/index.ts
+++ src/core/store/index.ts
@@ -317,12 +317,7 @@ namespace Store {
       value: string;
       trending?: boolean;
     }
-
-    export interface Product {
-        // todo add type
-        id: string;
-      }
-    }
+  }
 
   export interface SelectableList<T> {
     items: T[];
