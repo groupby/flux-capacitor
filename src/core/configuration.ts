78c1f6a743157061ef853339cdc6c1703653ad13
Mengqi <mengqi.zhu@groupbyinc.com>
2017-12-12 16:19:27 -0500

    review changes

--- src/core/configuration.ts
+++ src/core/configuration.ts
@@ -221,7 +221,7 @@ namespace Configuration {
     location?: Configuration.Recommendations.Location;
 
     /**
-     * Product ID field as used in recommendations.`
+     * Product ID field as used in recommendations.
      */
     idField: string;
     productSuggestions: Configuration.Recommendations.ProductSuggestions;
