7195fdf010ea20cb65a6996949413b216b685c31
Bruce Chen <bruce.chen@groupbyinc.com>
2017-12-21 14:42:23 -0500

    Lowercase comment

--- src/core/reducers/data/productsLoaded.ts
+++ src/core/reducers/data/productsLoaded.ts
@@ -4,7 +4,7 @@ import Store from '../../store';
 export type Action = Actions.ReceiveProductRecords;
 export type State = boolean;
 
-// TODO: Refactor storefront to avoid this pattern
+// todo: Refactor storefront to avoid this pattern
 // currently used to avoid race conditions with components on initialization
 export default function updateProductsLoaded(state: State = false, action: Action): State {
   switch (action.type) {
